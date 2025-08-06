3a4a92f9e73e91c54578c03527900a20e4b72881
Harsh Shukla <121473719+harshsu7@users.noreply.github.com>
2025-08-06 09:45:10 +0000

    Added Login Page and created a flask app for hosting the login page.

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug = True)
+
+
+
