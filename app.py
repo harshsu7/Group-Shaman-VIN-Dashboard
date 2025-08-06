254d40aeed1713d23009ba33203ba7a98311f4c1
Harsh Shukla <121473719+harshsu7@users.noreply.github.com>
2025-08-06 10:18:57 +0000

    Added Requirements

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def login():
-    return render_template("login.html")
+  return render_template("login.html")
+
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug = True)
-
-
-
+  app.run(host='0.0.0.0', debug=True)
